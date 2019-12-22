d6e734a4337fd217a8bf896b0d493f2c47b7c769
Balint Morasz <balint@morasz.hu>
2019-12-22 11:37:41 +0100

    minisuave

---
+++ minisuave/MiniSuave/MiniSuave.fs
@@ -0,0 +1,33 @@
+module MiniSuave
+
+open Suave.Http
+open Suave.Console
+open Suave.Successful
+open Suave.Combinators
+open Suave.Filters
+
+[<EntryPoint>]
+let main argv =
+    let request =
+        { Route = ""
+          Type = Suave.Http.GET }
+
+    let response =
+        { Content = ""
+          StatusCode = 200 }
+
+    let context =
+        { Request = request
+          Response = response }
+
+    let app =
+        Choose
+            [ GET >=> Path "/hello" >=> OK "Hello GET"
+              POST >=> Path "/hello" >=> OK "Hello POST"
+              Path "/foo" >=> Choose
+                                  [ GET >=> OK "Foo GET"
+                                    POST >=> OK "Foo POST" ] ]
+
+    executeInLoop context app
+
+    0 // return an integer exit code
